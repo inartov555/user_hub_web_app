b92b4e86371a9769914603c248d103a7ad7802e8
Iurii Nartov <yurahttp@gmail.com>
2025-10-11 20:42:51 +0300

    Fixing ImportError: Module profiles.serializers does not define a UserSerializer attribute/class

---
+++ backend/profiles/serializers/__init__.py
@@ -0,0 +1,5 @@
+from .user_serializer import UserSerializer
+from .profile_serializer import ProfileSerializer
+from .profile_update_serializer import ProfileUpdateSerializer
+
+__all__ = ["UserSerializer", "ProfileSerializer", "ProfileUpdateSerializer"]
