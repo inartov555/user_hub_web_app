930bfae371d776c4ac4c39b4e9421f3ad64e6b44
Iurii Nartov <yurahttp@gmail.com>
2025-10-28 21:35:35 +0200

    Implement the custom refresh serializer

--- backend/profiles/serializers/__init__.py
+++ backend/profiles/serializers/__init__.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-module-docstring
 
 from .change_password_serializer import ChangePasswordSerializer
+from .jwt_refresh_serializer.py import CustomTokenRefreshSerializer
 from .user_create_serializer import UserCreateSerializer
 from .email_or_user_token_create_serializer import EmailOrUsernameTokenCreateSerializer
 from .user_serializer import UserSerializer
@@ -8,6 +9,6 @@ from .profile_serializer import ProfileSerializer
 from .profile_update_serializer import ProfileUpdateSerializer
 
 
-__all__ = ["ChangePasswordSerializer", "UserCreateSerializer",
+__all__ = ["ChangePasswordSerializer", "CustomTokenRefreshSerializer", "UserCreateSerializer",
            "EmailOrUsernameTokenCreateSerializer", "UserSerializer", "ProfileSerializer",
            "ProfileUpdateSerializer"]
