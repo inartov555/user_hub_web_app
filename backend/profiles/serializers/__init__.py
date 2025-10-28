ca4295fc4ecbd3c8d266f4abca07f6a3951f4cee
Iurii Nartov <yurahttp@gmail.com>
2025-10-28 21:46:10 +0200

    Fixing: ModuleNotFoundError: No module named 'profiles.serializers.jwt_refresh_serializer.py'; 'profiles.serializers.jwt_refresh_serializer' is not a package

--- backend/profiles/serializers/__init__.py
+++ backend/profiles/serializers/__init__.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-module-docstring
 
 from .change_password_serializer import ChangePasswordSerializer
-from .jwt_refresh_serializer.py import CustomTokenRefreshSerializer
+from .jwt_refresh_serializer import CustomTokenRefreshSerializer
 from .user_create_serializer import UserCreateSerializer
 from .email_or_user_token_create_serializer import EmailOrUsernameTokenCreateSerializer
 from .user_serializer import UserSerializer
