cb43fce464243180f4b975a5ab4267144857d058
Iurii Nartov <yurahttp@gmail.com>
2025-10-29 02:45:21 +0200

    Fixing backend localization not changed

--- frontend/src/lib/i18n.ts
+++ frontend/src/lib/i18n.ts
@@ -4,8 +4,8 @@ import LanguageDetector from "i18next-browser-languagedetector";
 
 // Static JSON import (keeps tree-shaking simple)
 // For many languages, swap to lazy loading with i18next-http-backend.
-import en_US from "../locales/en_US.json";
-import et_EE from "../locales/et_EE.json";
+import en_US from "../locale/en_US.json";
+import et_EE from "../locale/et_EE.json";
 
 i18n
   .use(LanguageDetector) // detects from localStorage, navigator, querystring, etc.
