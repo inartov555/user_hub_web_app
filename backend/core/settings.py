8a5ee7b571b3631df5771828de519801eed278f8
Iurii Nartov <yurahttp@gmail.com>
2025-11-04 23:19:01 +0200

    Django CI: unit tests - session issues

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -243,8 +243,6 @@ CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "core.jwt_authentication.JWTAuthentication",
     ),
