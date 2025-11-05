59a2d52695938f990d2371d20feb0090155d95a4
Iurii Nartov <yurahttp@gmail.com>
2025-11-05 02:12:36 +0200

    frontend: adding localization to a button

--- frontend/src/components/PasswordInput.tsx
+++ frontend/src/components/PasswordInput.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useTranslation } from "react-i18next";
 
 type PasswordInputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, "type"> & {
   label?: React.ReactNode;
@@ -8,6 +9,7 @@ type PasswordInputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, "typ
 };
 
 export default function PasswordInput({ label, error, className = "", id, ...props }: PasswordInputProps) {
+  const { t } = useTranslation();
   const [show, setShow] = React.useState(false);
   return (
     <label className="block space-y-1">
@@ -15,6 +17,7 @@ export default function PasswordInput({ label, error, className = "", id, ...pro
       <div className="relative">
         <input
           id={id}
+          maxLength={40}
           type={show ? "text" : "password"}
           className={`w-full rounded-md border px-3 py-2 pr-10 outline-none focus:ring ${className}`}
           {...props}
@@ -25,7 +28,7 @@ export default function PasswordInput({ label, error, className = "", id, ...pro
           className="absolute right-2 top-1/2 -translate-y-1/2 text-sm"
           onClick={() => setShow((s) => !s)}
         >
-          {show ? "Hide" : "Show"}
+          {show ? <>{t("passwordComponent.show")}</> : <>{t("passwordComponent.hide")}</>}
         </button>
       </div>
       {error ? <span className="text-xs text-red-600">{error}</span> : null}
