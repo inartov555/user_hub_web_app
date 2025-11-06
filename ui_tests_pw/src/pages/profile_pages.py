1b085c259c0dd23180866ee0e19767f324d7dc53
Iurii Nartov <yurahttp@gmail.com>
2025-11-06 07:22:16 +0200

    PyLint: Error: Process completed with exit code 4.

--- ui_tests_pw/src/pages/profile_pages.py
+++ ui_tests_pw/src/pages/profile_pages.py
@@ -27,7 +27,7 @@ class ProfileEditPage(BasePage):
     """
     Profile Edit page
     """
-    def __init__(self, page: Page, base_url: str):
+    def __init__(self, page: Page, base_url: str):  # pylint: disable=useless-parent-delegation
         super().__init__(page, base_url)
 
     def open(self) -> None:
