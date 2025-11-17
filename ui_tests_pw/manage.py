371db4551f2e773a91ffe55511bd45006903c2ca
Iurii Nartov <yurahttp@gmail.com>
2025-11-17 16:55:50 +0200

    Django CI: Error: Process completed with exit code 2.

---
+++ ui_tests_pw/manage.py
@@ -0,0 +1,20 @@
+"""
+App launcher
+"""
+
+import os
+import sys
+
+from django.core.management import execute_from_command_line
+
+
+def main():
+    """
+    App launcher
+    """
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
