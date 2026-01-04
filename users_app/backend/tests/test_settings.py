fa4c363a664a613d2b5758bc9f4139ba2568816c
Iurii Nartov <yurahttp@gmail.com>
2026-01-04 13:26:54 +0200

    Update test_settings.py

--- users_app/backend/tests/test_settings.py
+++ users_app/backend/tests/test_settings.py
@@ -7,7 +7,7 @@ Unit tests
 
 from core.settings import *
 
-# Testing if Settings object can be initialized
+# Testing if the Settings object can be initialized
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3","NAME": ":memory:"}}
 PASSWORD_HASHERS=["django.contrib.auth.hashers.MD5PasswordHasher"]
 
