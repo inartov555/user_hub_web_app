4486484d592dd0cf8869d9719e53e909f88ff7fd
Iurii Nartov <yurahttp@gmail.com>
2026-01-04 13:21:04 +0200

    Update urls.py

--- users_app/backend/core/urls.py
+++ users_app/backend/core/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     path("api/v1/auth/", include("djoser.urls.jwt")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
