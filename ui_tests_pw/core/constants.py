1a2703513cf8a25f46862b3f15e9072510ec5388
Iurii Nartov <yurahttp@gmail.com>
2026-01-06 00:32:41 +0200

    Update constants.py

--- ui_tests_pw/core/constants.py
+++ ui_tests_pw/core/constants.py
@@ -4,7 +4,7 @@ Constants
 
 class LocaleConsts:
     """
-    Locale constants
+    Localization constants
     """
     ENGLISH_US = "en-US"
     UKRAINIAN= "uk-UA"
@@ -14,7 +14,7 @@ class LocaleConsts:
     POLISH = "pl-PL"
     SPANISH = "es-ES"
 
-    # Locales available in the website
+    # Locales available on the website
     ALL_AVAILABLE_LOCALES = [ENGLISH_US, UKRAINIAN, ESTONIAN, FINNISH, CZECH, POLISH, SPANISH]
     # Locales supported by the automation framework for testing
     ALL_SUPPORTED_LOCALES = ALL_AVAILABLE_LOCALES
@@ -27,7 +27,7 @@ class ThemeConsts:
     LIGHT = "light"
     DARK = "dark"
 
-    # Themes available in the website
+    # Themes available on the website
     ALL_AVAILABLE_THEMES = [LIGHT, DARK]
     # Themes supported by the automation framework for testing
     ALL_SUPPORTED_THEMES = ALL_AVAILABLE_THEMES
