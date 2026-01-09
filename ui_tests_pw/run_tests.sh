a018476ff7d4b153af3f4a80994b27bcff32c41b
Iurii Nartov <yurahttp@gmail.com>
2026-01-09 18:29:43 +0200

    Revert "Fixing: AttributeError: 'UsersTablePage' object has no attribute 'assert_change_password_is_loaded'"
    This reverts commit 359e1b68e6b54c2500434116b8abf7bdaf9b9322.

--- ui_tests_pw/run_tests.sh
+++ ui_tests_pw/run_tests.sh
@@ -104,7 +104,7 @@ esac
 echo "Starting the tests..."
 TEST_GREP=""
 
-# TEST_GREP="$TEST_GREP --reruns 2 --reruns-delay 2"
+TEST_GREP="$TEST_GREP --reruns 2 --reruns-delay 2"
 
 # Uncomment if you need tests to be run in parallel (set number or auto).
 # Note: there are some tests that change settings, so you can run tests in parallel only when
@@ -116,7 +116,7 @@ TEST_GREP=""
 # to run all tests, just set TEST_GREP="$TEST_GREP" (to preserve base settings)
 
 # TEST_GREP="$TEST_GREP -k 'test_base_demo or test_locale_demo'"
-TEST_GREP="$TEST_GREP -k 'test_check_that_updates_from_excel_are_applied'"
+# TEST_GREP="$TEST_GREP -k 'test_user_is_present_in_user_stats_after_logging_out'"
 
 docker compose run -e TEST_GREP="$TEST_GREP" --rm ui_tests_pw
 
