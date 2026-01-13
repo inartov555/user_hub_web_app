07e35085de2a3eb1d83ca8dd4122903e761588af
Iurii Nartov <yurahttp@gmail.com>
2026-01-13 12:23:05 +0200

    API tests

---
+++ api_tests/core/app_config.py
@@ -0,0 +1,20 @@
+"""
+App config from the pytest.ini config file
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass(slots=True)
+class AppConfig:
+    """
+    App config from ini config file
+    """
+    action_timeout: int
+    navigation_timeout: int
+    assert_timeout: int
+    browser: str
+    base_url: str
+    is_headless: bool
+    width: int
+    height: int
