3278cdab8a0852e3d7109636d1f1c76ffb6b3d11
Iurii Nartov <yurahttp@gmail.com>
2026-01-13 12:58:59 +0200

    API tests

--- api_tests/conftest.py
+++ api_tests/conftest.py
@@ -4,11 +4,38 @@ conftest.py
 
 import os
 import httpx
+from configparser import ConfigParser, ExtendedInterpolation
 
 import pytest
 
+from core.app_config import AppConfig
+
 
 DEFAULT_BASE_URL = "http://localhost:8000"
+
+
+@pytest.fixture(scope="session")
+def app_config(pytestconfig) -> AppConfig:
+    """
+    Set and get AppConfig from ini config
+    """
+    ini_config_file = pytestconfig.getoption("--ini-config")
+    log.info(f"Reading config properties from '{ini_config_file}' and storing to a data class")
+    result_dict = {}
+    cfg = ConfigParser(interpolation=ExtendedInterpolation())
+    cfg.read(ini_config_file)
+    result_dict["action_timeout"] = cfg.getfloat("pytest", "action_timeout", fallback=15000.0)
+    result_dict["navigation_timeout"] = cfg.getfloat("pytest", "navigation_timeout", fallback=15000.0)
+    result_dict["assert_timeout"] = cfg.getfloat("pytest", "assert_timeout", fallback=15000.0)
+    result_dict["browser"] = cfg.get("pytest", "browser", fallback="chromium")
+    result_dict["base_url"] = \
+        cfg.get("pytest",
+                "base_url",
+                fallback="%s:%s" % (UI_BASE_URL, UI_BASE_PORT))  # pylint: disable=consider-using-f-string
+    result_dict["is_headless"] = cfg.getboolean("pytest", "is_headless", fallback=False)
+    result_dict["width"] = cfg.getint("pytest", "width", fallback=1920)
+    result_dict["height"] = cfg.getint("pytest", "height", fallback=1080)
+    return AppConfig(**result_dict)
 
 
 @pytest.fixture(name="base_url", scope="session")
