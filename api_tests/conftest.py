5f09a0bfd3a6ffa7b4315618503b037222da5035
Iurii Nartov <yurahttp@gmail.com>
2026-01-13 13:07:21 +0200

    PyLint:  Error: Process completed with exit code 16.

--- api_tests/conftest.py
+++ api_tests/conftest.py
@@ -37,12 +37,3 @@ def base_url_fixture() -> str:
     Get base URL
     """
     return os.getenv("API_BASE_URL", DEFAULT_BASE_URL).rstrip("/")
-
-
-@pytest.fixture(name="client", scope="session")
-def client_fixture(base_url: str):
-    """
-    Get client
-    """
-    with httpx.Client(base_url=base_url, timeout=30.0, follow_redirects=True) as _client:
-        yield _client
